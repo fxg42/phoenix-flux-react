0558c7dfd4c06695e4218dfb9c7c62168bcfd585
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2015-02-09 14:14:40 -0500

    remove mixin; inline subitem rendering

--- web/js/components/QuoteChart.react.js
+++ web/js/components/QuoteChart.react.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Chartist from 'chartist'
-import ImmutableRenderMixin from 'react-immutable-render-mixin'
 
 export default class QuoteChart extends React.Component {
-  mixins: [ ImmutableRenderMixin ]
 
   componentWillReceiveProps(props) {
     return this.updateChart(props)
