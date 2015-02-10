7d6bd5a1a29cce9be141111e3b50bd4e56c13315
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2015-02-09 21:31:42 -0500

    reindent...

--- web/js/components/QuoteList.react.js
+++ web/js/components/QuoteList.react.js
@@ -4,10 +4,16 @@ import Quote from './Quote.react'
 export default class QuoteList extends React.Component {
 
   render() {
-    return <ul style={{textAlign: 'left'}}>{
-      this.props.data.quotes.toList().sortBy(q => q.get('value')).map(q =>
-        <Quote key={q.get('name')} name={q.get('name')} value={q.get('value')}/>
-      )}
-    </ul>
+    return (
+      <ul style={{textAlign: 'left'}}>{
+        this.props.data.quotes
+          .toList()
+          .sortBy(q => q.get('value'))
+          .map(q =>
+            <Quote key={q.get('name')}
+                   name={q.get('name')}
+                   value={q.get('value')} />)
+      }</ul>
+    )
   }
 }
